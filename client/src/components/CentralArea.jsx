068cb8365b46c4034f2fbdfd67bd57e4386f9aaf
Mark <markctchu@gmail.com>
2026-03-24 17:38:33 +0800

    UI: Fix contact attempt countdown bar animation and width stability

--- client/src/components/CentralArea.jsx
+++ client/src/components/CentralArea.jsx
@@ -242,8 +242,8 @@ function CentralArea() {
                   {isContactAttempt && (
                     <div className="absolute bottom-0 left-0 w-full h-1.5 bg-surface-container overflow-hidden">
                       <div 
-                        className="h-full bg-on-secondary-container transition-all duration-1000 linear"
-                        style={{ width: `${(currentGuess.countdown / 10) * 100}%` }}
+                        className="h-full bg-on-secondary-container transition-all duration-1000 ease-linear"
+                        style={{ width: `${((currentGuess.countdown || 0) / 10) * 100}%` }}
                       ></div>
                     </div>
                   )}
